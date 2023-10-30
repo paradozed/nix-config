d8f23e650fd15c96093d2bdaefcdaa89c664a61c
Nicolas <nicolas.bouzin@tutanota.com>
2023-10-30 16:13:37 +0100

    add traefik podman jellyfin

--- configuration.nix
+++ configuration.nix
@@ -67,8 +67,6 @@
    iperf3
    neofetch
    ncdu
-   traefik
-   jellyfin
   ];
 
   # programs.mtr.enable = true;
@@ -80,12 +78,7 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-  services.jellyfin.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
   networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
